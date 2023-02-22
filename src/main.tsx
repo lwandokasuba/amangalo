526186b8376f4fc8d5ba98cd225960eb5036a012
lwandokasuba <lwando@thinkzambia.org>
2023-02-22 15:40:36 +0200

    Added routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import {
+	createBrowserRouter,
+	RouterProvider,
+} from 'react-router-dom';
+import ErrorPage from './error-page';
+import Home from './routes/home';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: '/',
+				element: <Home />,
+			},
+		],
+	},
+	{
+		path: '/test',
+		element: <div>Test</div>,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
