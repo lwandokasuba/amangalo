9030e734c47fdfd2d71d9fe98b3eea54c5f081f9
lwandokasuba <lwando@thinkzambia.org>
2023-02-22 15:41:51 +0200

    Root route

---
+++ src/routes/root.tsx
@@ -0,0 +1,12 @@
+import {Outlet} from 'react-router-dom';
+import {Footer, NavBar} from '../components';
+
+export default function Root() {
+	return (
+		<>
+			<NavBar />
+			<Outlet />
+			<Footer />
+		</>
+	);
+}
