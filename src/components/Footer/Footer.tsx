0e72f72712a4f65183609359daa4423297e8eb31
lwandokasuba <lwando@thinkzambia.org>
2023-02-22 15:40:57 +0200

    Added footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,35 @@
+
+// Generate a footer using Material UI that is attached to the bottom of the page
+// and contains a link to the GitHub repository for this project.
+
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import LinkMui from '@mui/material/Link';
+
+export function Footer() {
+	return (
+		<footer style={{
+			position: 'absolute',
+			bottom: 0,
+			width: '100%',
+			height: '60px',
+			color: 'white',
+			backgroundColor: 'black',
+		}}>
+			<Toolbar>
+				<Typography variant='body1' color='textSecondary' align='center'>
+					<LinkMui
+						component={Link}
+						to='https://github.com/lwandokasuba/amangalo'
+						color='white'
+						underline='none'
+					>
+            Amangalo
+					</LinkMui>
+				</Typography>
+			</Toolbar>
+		</footer>
+	);
+}
