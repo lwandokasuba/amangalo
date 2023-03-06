87ae1ffaff1a1ad099f6ad4cec1797e1384ab024
lwandokasuba <lwando@thinkzambia.org>
2023-03-06 02:15:18 +0200

    firebase app initializer values fetching from .env

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
