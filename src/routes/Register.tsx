e79ef939fab8bc552b3278ddfc70c554151dc4e6
lwandokasuba <lwando@thinkzambia.org>
2023-03-06 02:17:57 +0200

    Register page

---
+++ src/routes/Register.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Register() {
+  return (
+    <>
+      <Toolbar />
+      <Typography variant="h6" align="center" component="div" sx={{ flexGrow: 1 }}>Register page</Typography>
+      <Toolbar />
+    </>
+  );
+}
