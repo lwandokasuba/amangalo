a5a354479466a86351ce1adeb36d958dd1ff83d4
lwandokasuba <lwando@thinkzambia.org>
2023-03-06 02:15:48 +0200

    added DataProvider and moved routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import App from './App';
-import ErrorPage from './error-page';
-import Home from './routes/home';
-import Root from './routes/root';
+import Root from './routes';
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-    ],
-  },
-  {
-    path: '/test',
-    element: <App />,
-  },
-]);
+import { DataProvider } from './context';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <DataProvider>
+      <Root />
+    </DataProvider>
   </React.StrictMode>,
 );
